10e3d9c92fcf26c87d678a7642feaa3b693f9577
Feven-Tolosa <tolosafeven0@gmail.com>
2025-08-07 16:02:03 +0300

    Product Data

---
+++ src/data/products.ts
@@ -0,0 +1,26 @@
+export const furnitureProducts: Product[] = [
+  {
+    id: 1,
+    name: 'Nordic Oak Sofa',
+    price: 1299,
+    rating: 4,
+    material: 'Solid Oak',
+    image: '/sofa-nordic.jpg',
+  },
+  {
+    id: 2,
+    name: 'Industrial Bar Stool',
+    price: 189,
+    rating: 5,
+    material: 'Steel/Walnut',
+    image: '/bar-stool.jpg',
+  },
+  {
+    id: 3,
+    name: 'Scandi Platform Bed',
+    price: 899,
+    rating: 4,
+    material: 'Pine Wood',
+    image: '/platform-bed.jpg',
+  },
+]
